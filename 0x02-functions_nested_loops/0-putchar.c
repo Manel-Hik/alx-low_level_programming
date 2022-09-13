1366449cc7d4a2b07f5d4af64c38f3cbbf9d39bf
Manel-Hik <manelaloui2@gmail.com>
2022-09-13 02:17:56 -0700

    TASK0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
+int main(void)
+{
+printf("_putchar\n")
+return(0)
+}
