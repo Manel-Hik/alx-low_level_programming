707789cbfe2f0eb3caea1ee459dffc3435d59f39
Manel-Hik <manelaloui2@gmail.com>
2022-09-15 10:24:12 -0700

    TASK5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,23 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 *more_numbers - function that print all numbers from
 *0 to 14
 *
 *Return: returns nothing
 */
-
 void more_numbers(void)
 {
-int i, j;
+	int i, j;
 
-for (j = 0; j <= 9; j++)
-{
-for (i = 0; i <= 14; i++)
-{
-_putchar(i + '0');
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			_putchar(i + '0');
 
-}
-_putchar('\n');
-}
+		}		
+		_putchar('\n');
+	}
 }
