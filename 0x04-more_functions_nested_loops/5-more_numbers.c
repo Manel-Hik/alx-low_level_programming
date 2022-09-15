3eb183295fdd75799ee495d1049a11cd3a08d8e6
Manel-Hik <manelaloui2@gmail.com>
2022-09-15 10:28:09 -0700

    TASK5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,17 @@
 *
 *Return: returns nothing
 */
+
 void more_numbers(void)
 {
-	int i, j;
+int i, j;
 
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 0; i < 15; i++)
-		{
-			_putchar(i + '0');
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 15; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
 
-		}		
-		_putchar('\n');
-	}
 }
