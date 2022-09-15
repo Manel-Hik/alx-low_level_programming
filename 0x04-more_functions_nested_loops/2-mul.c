8fae2b6d191df045e6ad7d6617447f578b6ec42b
Manel-Hik <manelaloui2@gmail.com>
2022-09-15 07:33:33 -0700

    Update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,7 +6,7 @@
  * @b : An integer to be multipled with a
  * Return: Return the result of the multiplication
  */
-int mul (int a, int b)
+int mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
