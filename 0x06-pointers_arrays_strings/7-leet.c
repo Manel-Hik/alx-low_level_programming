86004d0b022ed0076b3da812c95e13c3fbe256bf
Manel-Hik <manelaloui2@gmail.com>
2022-09-22 09:05:43 -0700

    TASK7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,35 +3,32 @@
 /**
  * *leet - check the code for Holberton School students.
  *
- * @str: string.
+ * @c: pointer to char params.
  * Return: string.
  */
-char *leet(char *str)
-{
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			str[i] = '4';
+			/*32 is the difference between lower case letters and apper case letters*/
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
 		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-		}
+		c++;
 	}
-	return (str);
+
+	return (cp);
+
 }
+Footer
+
