06827eeec13752af8b36664f5bc6288771dfe3b0
Manel-Hik <manelaloui2@gmail.com>
2022-09-22 02:38:27 -0700

    Task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,18 @@
 
 char *_strcat(char *dest, char *src)
 {
+int length, j;
 
-	int index = 0, dest_len = 0;
+length = 0;
+while (dest[length] != '\0')
+{
+        ++length;
+}
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+for (j = 0; src[j] != '\0'; ++j)
+{
+dest[length] = src[j];
+}
+dest[length] = '\0';
+return (dest);
 }
