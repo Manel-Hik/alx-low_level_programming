163d96bd63991d366fb0eac431b8ac8b635c2139
Manel-Hik <manelaloui2@gmail.com>
2022-09-22 08:46:27 -0700

    fix

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int length, j, n;
+
+length = 0;
+
+while (dest[length] != '\0')
+{
+length++;
+}
+for (j = 0; src[j] != '\0' && j < n; j++)
+{
+dest[length] = src[j];
+length++;
+}
+dest[length] = '\0';
+
+return (dest);
+}
