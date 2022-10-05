62c46cc56407300b529e922bc7cf5b2ff5d5973c
Manel-Hik <manelaloui2@gmail.com>
2022-10-05 00:37:42 -0700

    arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,40 +1,23 @@
 #include "main.h"
-
 /**
- * *leet - check the code for Holberton School students.
- *
- * @str: string.
- * Return: string.
- */
+* leet - function that encode a string
+*@str:string that will be encoded
+*Return:returns encoded string
+*/
 
 char *leet(char *str)
 {
-	int i;
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[++index1])
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (index2 = 0; index2 <= 7; index2++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[index1] == leet[index2] ||
+			 str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
 		}
 	}
 	return (str);
 }
-
-
